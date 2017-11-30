69ced1e1f7aa22dfa86ce97ad64cb7f27a8c626c
Yangxg <jessebutchman@gmail.com>
2017-12-01 00:49:47 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <radio-plus v-model="radio1" value="1" style="margin-right:20px;"/>
+    <radio-plus v-model="radio1" value="2"/>
+  </div>
+</template>
+
+<script>
+import radioPlus from './radioPlus'
+export default {
+  components:{
+      radioPlus
+    },
+  data () {
+    return {
+      radio1:"",
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/radioPlus.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="radio_wrap" @click="checkedM" :class="{checked:isChecked}">
+    <input type="radio" name="abc"
+    ref="radio"
+        @change="updateValue"
+        :value="value"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props:['value','checked'],
+  data () {
+    return {
+        isChecked:false
+    }
+  },methods:{
+      checkedM(){
+          this.$refs.radio.checked = true
+          this.updateValue()
+      },
+      updateValue: function () {
+        //   console.log('updateValue')
+        //   this.$emit('value', value)
+        // var formattedValue = value
+        //     // 删除两侧的空格符
+        //     .trim()
+        //     // 保留 2 位小数
+        //     .slice(
+        //     0,
+        //     value.indexOf('.') === -1
+        //         ? value.length
+        //         : value.indexOf('.') + 3
+        //     )
+        // // 如果值尚不合规，则手动覆盖为合规的值
+        // if (formattedValue !== value) {
+        //     this.$refs.input.value = formattedValue
+        // }
+        // // 通过 input 事件带出数值
+        console.log(this.$refs.radio.checked)
+        if(!this.$refs.radio.checked){
+            this.isChecked = false
+        }else{
+            this.isChecked = true
+        }
+        this.$emit('change', this.value)
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.radio_wrap{
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background-color: #ddd;
+}
+.checked{
+    background-color:#42b983;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
